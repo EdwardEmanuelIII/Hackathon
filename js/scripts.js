f24d96ee5de56eccd011fa71c213000979cee674
Edward Emanuel <edward.emanuel@gmail.com>
2025-07-04 15:04:15 -0500

    Count down

--- js/scripts.js
+++ js/scripts.js
@@ -64,11 +64,16 @@ var x = setInterval(function () {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24))
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  //   var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
   // Display the result in the element with id="demo"
-  document.getElementById('timer').innerHTML =
-    days + ' days ' + hours + ' hours ' + minutes + ' minutes '
+  //   document.getElementById('timer').innerHTML =
+  //     days + ' : ' + hours + ' : ' + minutes
+
+  document.getElementById('days').innerHTML = days
+  document.getElementById('hours').innerHTML = hours
+  document.getElementById('minutes').innerHTML = minutes
+  document.getElementById('seconds').innerHTML = seconds
 
   // If the count down is finished, write some text
   if (distance < 0) {
